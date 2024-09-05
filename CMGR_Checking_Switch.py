0c1bd40555f6102d0e29a69b0c07f8251d106a78
JackyLin213 <linpogin213@gmail.com>
2024-09-05 21:22:27 +0800

    commit

--- CMGR_Checking_Switch.py
+++ CMGR_Checking_Switch.py
@@ -1,27 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
-import subprocess
+
+
+from Impedance_Checker_V12 import Impedance_Checker
+from Length_Checker_V12 import Length_Checker
+from Spacing_Checker_V12 import Spacing_Checker
+
 
 def run_script(script_name):
-    
     try:
-        subprocess.Popen(["python", script_name])
+        if script_name == "Impedance_Checker":
+            Impedance_Checker()
+        elif script_name == "Length_Checker":
+            Length_Checker()
+        elif script_name == "Spacing_Checker":
+            Spacing_Checker()
+
     except Exception as e:
         tk.messagebox.showerror("Error", f"Failed to run script: {e}")
 
+
 root = tk.Tk()
 root.title("CMGR_Checking_Switch")
-root.geometry("400x100") 
+root.geometry("400x100")
 
 # 創建一個 Combobox 來選擇要執行的腳本
-script_options = ["Impedance_Checker_V1.1.py", "Length_Checker_V1.1.py", "Spacing_Checker_V1.1.py"]
+script_options = ["Impedance_Checker", "Length_Checker", "Spacing_Checker"]
 selected_script = tk.StringVar()
 script_combobox = ttk.Combobox(root, textvariable=selected_script, values=script_options, width=25)
 script_combobox.set(script_options[0])  # 預設選擇第一個腳本
 script_combobox.pack(pady=20)
 
 # 創建一個按鈕來執行選定的腳本
-run_button = ttk.Button(root, text="Run Script", command=lambda: run_script(selected_script.get()))
+run_button = ttk.Button(root, text="Run Function", command=lambda: run_script(selected_script.get()))
 run_button.pack()
 
 root.mainloop()
